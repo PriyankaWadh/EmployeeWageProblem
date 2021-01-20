22de2b2fea372c10759a2bb3fc2224d40f9d26f5
“PriyankaWadh” <wadhavanepriyanka@gmail.com>
2021-01-20 12:59:02 +0530

    Part time employee wage added

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
 empRatePerHr=20
-empHrs=8
+empFullTimeHrs=8
+empPartTimeHrs=4
 
 attendance_check=$(( RANDOM%2 ))
 
-if [[ $attendance_check -eq 1 ]]
+if [ $attendance_check -eq 1 ]
 then
 	echo employee is present
-	salary=$(( $empRatePerHr * $empHrs ))
+	salary=$(( $empRatePerHr * $empFullTimeHrs ))
+	halfSalary=$(( $empRatePerHr * $empPartTimeHrs ))
 else
 	echo employee is absent
 	salary=0
+	halfSalary=0
 fi
 
 echo "salary of an employee:" $salary
+echo "part time salary of an employee:" $halfSalary
