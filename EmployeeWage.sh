3f7b248eb653f580e5b18692ac82781c169d6c5e
“PriyankaWadh” <wadhavanepriyanka@gmail.com>
2021-01-20 13:03:14 +0530

    Using switch case

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -16,5 +16,21 @@ else
 	halfSalary=0
 fi
 
+empCheck=$(( RANDOM%3 + 1 ))
+case $empCheck in
+	1)
+	empHrs=8
+	;;
+	2)
+	empHrs=4
+	;;
+	3)
+	empHrs=0
+	;;
+esac
+
+employeeWage=$(( $empHrs * $empRatePerHr ))
+
 echo "salary of an employee:" $salary
 echo "part time salary of an employee:" $halfSalary
+echo "Employee Wage:" $employeeWage
