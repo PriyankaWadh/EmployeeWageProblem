fe444bb588e36ba211ba16c8538d4f7f683e0a39
“PriyankaWadh” <wadhavanepriyanka@gmail.com>
2021-01-20 12:54:47 +0530

    Daily employee wage calculated

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
+empRatePerHr=20
+empHrs=8
+
 attendance_check=$(( RANDOM%2 ))
-if [ $attendance_check -eq 1 ]
+
+if [[ $attendance_check -eq 1 ]]
 then
 	echo employee is present
+	salary=$(( $empRatePerHr * $empHrs ))
 else
 	echo employee is absent
+	salary=0
 fi
+
+echo "salary of an employee:" $salary
